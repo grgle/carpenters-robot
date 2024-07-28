0cffd5f0dd9ae102b2ab5e04ff4027913def2ae0
grgle <62070601+grgle@users.noreply.github.com>
2024-07-28 22:45:25 +0200

    change

--- docs/script.js
+++ docs/script.js
@@ -67,3 +67,12 @@ document.getElementById("messages").innerHTML += "<span> Message to topic "+topi
 
 
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
